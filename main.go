ace0b8899a24290f15ca4481ab565db88031450b
Carlos Zendejas <czendee@gmail.com>
2021-10-20 09:03:01 -0500

    logic to read files from git repository and modify their content

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"os"
 	"time"
+//         "io/fs"
+        "strings"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
@@ -27,7 +29,8 @@ import (
 
 // variables for the the in memory repository  and the filesystem to handle internally the local git repo
 var storer *memory.Storage
-var fs billy.Filesystem
+var fsmemory billy.Filesystem
+
 
 
 func main() {
@@ -123,10 +126,10 @@ func getMultiplicaByID(c *gin.Context) {
 //func addInGit(filenombre string )  bolean{
 func addInGit() {
 	
-	fmt.Println("addInGit 7th  delcare in memory")
+	fmt.Println("addInGit 8th  delcare in memory")
 	
         storer = memory.NewStorage()
-        fs = memfs.New()
+        fsmemory = memfs.New()
         fmt.Println("addInGit   set auth")
         // Authentication
         auth := &httpgit.BasicAuth{
@@ -135,14 +138,15 @@ func addInGit() {
 //                Password: "Impo",
 //                Password: "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",  
 //                Password: "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", 
-                Password: "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", 
+//                Password: "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",
+                Password: "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",
 
 
         }
 
 	fmt.Println("oct 5 addInGit   define github repository and login ")
         repository := "https://github.com/youtochibots/bot.git"
-        r, err := git.Clone(storer, fs, &git.CloneOptions{
+        r, err := git.Clone(storer, fsmemory, &git.CloneOptions{
                 URL:  repository,
                 Auth: auth,
         })
@@ -154,6 +158,65 @@ func addInGit() {
                 return 
         }
         fmt.Println("Repository cloned")
+        fmt.Println("Now display the contnets of the repository ")
+
+
+
+	// Getting the latest commit on the current branch
+        fmt.Println("Oct 19 -git log -1")
+
+	// ... retrieving the branch being pointed by HEAD
+	ref, err := r.Head()
+
+	// ... retrieving the commit object
+	commit01, err := r.CommitObject(ref.Hash())
+
+	fmt.Println(commit01)
+
+	// List the tree from HEAD
+        fmt.Println(" Oct 19 - git ls-tree -r HEAD")
+
+	// ... retrieve the tree from the commit
+	tree, err := commit01.Tree()
+
+	// ... get the files iterator and print the file
+        var file001content string
+
+	tree.Files().ForEach(func(f *object.File) error {
+//		fmt.Printf("100644 blob %s    %s\n", f.Hash, f.Name)
+                if(f.Name =="packages/leon/data/answers/en.json" ){
+                    fmt.Println(" Oct 19 - file001 is set ")
+                    file001content, err= f.Contents()
+	          fmt.Printf(f.Contents())
+
+                }
+		return nil
+	})
+
+ 
+
+        // Create new file  packages/leon/data/answers/en.json
+        filePath00 := "packages/leon/data/answers/en333.json"
+        newFile00, err := fsmemory.Create(filePath00)
+        if err != nil {
+                return 
+        }
+
+       resultsFile001 := strings.Replace(file001content, "Tochi.", "SOY TOCHI Y QUE", -1)
+
+        newFile00.Write( []byte(resultsFile001 )  ) 
+
+
+	if err := newFile00.Close(); err != nil {
+		return
+	}
+
+
+
+
+
+
+        fmt.Println("Repository cloned and now work with new files")
 
         w, err := r.Worktree()
         if err != nil {
@@ -163,13 +226,91 @@ func addInGit() {
 
 	fmt.Println("addInGit   create new file")
         // Create new file
-        filePath := "mipath/my-new-carlos004.txt"
-        newFile, err := fs.Create(filePath)
+        filePath := "mipath/my-new-carlos005_2.txt"
+        newFile, err := fsmemory.Create(filePath)
         if err != nil {
                 return 
         }
-        newFile.Write([]byte("My new file carlos004"))
+        newFile.Write([]byte("My new file carlos005"))
         newFile.Close()
+
+        // Create new file  packages/leon/data/answers/en.json
+        filePath01 := "packages/leon/data/answers/en_json_2.txt"
+        newFile01, err := fsmemory.Create(filePath01)
+        if err != nil {
+                return 
+        }
+        newFile01.Write([]byte(
+       "	\"miembrosfamiliaveracruz\": {  "+
+		" \"miembros_familia_veracruz\": [  "+
+		"	\"LEo.\",  "+
+		" \"Tommy.\",   "+
+	        " \"Bodoque.\",    "+
+         	" \"Tochi.\",   "+
+         	" \"Flaco.\"   "+
+	 	"	]   "+
+	 	"  },     "+
+         	"    \"partnerassistant\": {" )) 
+  
+        newFile01.Close()
+
+        // Create new file  packages/leon/data/answers/en.json
+        filePath02 := "packages/leon/data/expressions/en_json_2.txt"
+        newFile02, err := fsmemory.Create(filePath02)
+        if err != nil {
+                return 
+        }
+        newFile02.Write([]byte(
+
+		" \"miembrosfamiliaveracruz\": {  "+
+		" \"run\": {   "+
+		" \"expressions\": [  "+
+		" \"Member of the familiy in Veracruz?\",   "+
+		" \"Psrt of the Family in veracruz\",   "+
+		" \"Is part of the Family in veracruz\",  "+
+		" \"vive en depa Veracruz\",  "+
+		" \"Vive en Veracruz\"  "+
+		"  ]   "+
+		" }  "+
+		"},    "+
+		" \"partnerassistant\": {  "           )) 
+  
+        newFile02.Close()
+
+        // Create new file  packages/leon/config/config.sample.json
+        filePath03 := "packages/leon/config/config_sample_json_2.txt"
+        newFile03, err := fsmemory.Create(filePath03)
+        if err != nil {
+                return 
+        }
+        newFile03.Write([]byte(
+
+		" \"miembrosfamiliaveracruz\": {  "+
+		" \"options\": {}    "+
+		"  },   "+
+
+		" \"partnerassistant\": {  "          )) 
+  
+        newFile03.Close()
+
+
+        // Create new file  packages/leon/meaningoflife.py
+        filePath04 := "packages/leon/meaningoflife_py_2.txt"
+        newFile04, err := fsmemory.Create(filePath04)
+        if err != nil {
+                return 
+        }
+        newFile04.Write([]byte(
+		" #!/usr/bin/env python  \n "+
+		" # -*- coding:utf-8 -*-  \n "+
+		" import utils     \n"+
+		" def run(string, entities):    \n "+
+		" 	\"\"\"Leon saysthe family members in veracruz\"\"\"       \n"+
+		" 	return utils.output('end', 'miembros_familia_veracruz', utils.translate('miembros_familia_veracruz'))    "          )) 
+  
+        newFile04.Close()
+
+
 
         // Run git status before adding the file to the worktree
         fmt.Println(w.Status())
@@ -177,6 +318,26 @@ func addInGit() {
 	fmt.Println("addInGit   git add the file")
         // git add $filePath
         w.Add(filePath)
+
+	fmt.Println("addInGit   git add the file001")
+        // git add $filePath
+        w.Add(filePath01)
+
+	fmt.Println("addInGit   git add the file002")
+        // git add $filePath
+        w.Add(filePath02)
+
+	fmt.Println("addInGit   git add the file003")
+        // git add $filePath
+        w.Add(filePath03)
+
+	fmt.Println("addInGit   git add the file004")
+        // git add $filePath
+        w.Add(filePath04)
+
+	fmt.Println("addInGit   git add the file000 overwrite/edit")
+        // git add $filePath
+        w.Add(filePath00)
 
         // Run git status after the file has been added adding to the worktree
         fmt.Println(w.Status()) //displays A accoridng with doc this is add  https://pkg.go.dev/github.com/go-git/go-git/v5@v5.1.0#StatusCode
